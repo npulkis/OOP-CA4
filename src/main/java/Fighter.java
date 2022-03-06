4be0d66aa98de71f3e5749e20ff07864b59b2d53
Nathan <nathanpulkis@gmail.com>
2022-03-06 12:19:01 +0000

    Added Fighter Class

---
+++ src/main/java/Fighter.java
@@ -0,0 +1,11 @@
+public class Fighter {
+    private String name;
+    private int totalFights;
+    private double winLoseRatio;
+
+    Fighter(String name, int totalFights, double winLoseRatio){
+        this.name = name;
+        this.totalFights= totalFights;
+        this.winLoseRatio = winLoseRatio;
+    }
+}
