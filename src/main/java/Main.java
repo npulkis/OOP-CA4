39ac8c7b76ff09a93dd8d1d538f7862003541e81
Nathan <nathanpulkis@gmail.com>
2022-03-06 13:35:27 +0000

    Completed Part 2 - Provide menu option to display all elements;

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,17 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Main {
-   public static void main(String[] args) {
 
+   ArrayList<Fighter> fighters;
+
+   public static void main(String[] args) {
+      Main app = new Main();
+      app.start();
+   }
+
+   public void start(){
       Fighter fighter1 = new Fighter("Israel Adesanya",22,1);
       Fighter fighter2 = new Fighter("Conor McGregor",22,6);
       Fighter fighter3 = new Fighter("Dustin Poirier",28,7);
@@ -14,19 +23,69 @@ public class Main {
       Fighter fighter9 = new Fighter("Holly Holm",14,5);
       Fighter fighter10 = new Fighter("Jorge Masvidal",35,16);
 
-      ArrayList<Fighter> Fighters = new ArrayList<>();
-      Fighters.add(fighter1);
-      Fighters.add(fighter2);
-      Fighters.add(fighter3);
-      Fighters.add(fighter4);
-      Fighters.add(fighter5);
-      Fighters.add(fighter6);
-      Fighters.add(fighter7);
-      Fighters.add(fighter8);
-      Fighters.add(fighter9);
-      Fighters.add(fighter10);
 
+       fighters = new ArrayList<>();
+      fighters.add(fighter1);
+      fighters.add(fighter2);
+      fighters.add(fighter3);
+      fighters.add(fighter4);
+      fighters.add(fighter5);
+      fighters.add(fighter6);
+      fighters.add(fighter7);
+      fighters.add(fighter8);
+      fighters.add(fighter9);
+      fighters.add(fighter10);
+
+
+      displayMainMenu();
 
 
    }
-}
+
+   public void displayMainMenu() {
+
+      final String MENU_ITEMS = "\n*** Main Menu ***\n"
+              + "1. Display all ArrayList elements\n"
+              + "2. Exit\n"
+              + "Enter Option [1,2]\n";
+
+      final int DISPLAY_ARRAYLIST = 1;
+      final int EXIT = 2;
+
+      Scanner keyboard = new Scanner(System.in);
+      int option = 0;
+
+      do {
+         System.out.println("\n" + MENU_ITEMS);
+         try {
+            String userInput = keyboard.nextLine();
+            option = Integer.parseInt(userInput);
+
+            switch (option) {
+               case DISPLAY_ARRAYLIST:
+                  System.out.println("Display all ArrayList elements chosen\n");
+                  displayArrayList(fighters);
+                  break;
+               case EXIT:
+                  System.out.println("Exit menu option chosen");
+                  break;
+               default:
+                  System.out.println("Invalid Option - please enter number in range");
+                  break;
+            }
+
+         } catch (InputMismatchException | NumberFormatException e) {
+            System.out.print("Invalid option - please enter number in range");
+         }
+      } while (option != EXIT);
+
+      System.out.println("\nExiting Main Menu, goodbye.");
+   }
+
+   public void displayArrayList(ArrayList<Fighter> fighters){
+
+      for (Fighter fighter : fighters) {
+         System.out.println(fighter);
+      }
+   }
+   }
