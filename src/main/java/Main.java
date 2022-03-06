adad6ea6bb0bf9ff506ead5f66db8cdb8d65d279
Nathan <nathanpulkis@gmail.com>
2022-03-06 13:00:59 +0000

    Completed Part 1 - Instantiate an ArrayList and populate it;

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,32 @@
+import java.util.ArrayList;
 
 public class Main {
+   public static void main(String[] args) {
 
+      Fighter fighter1 = new Fighter("Israel Adesanya",22,1);
+      Fighter fighter2 = new Fighter("Conor McGregor",22,6);
+      Fighter fighter3 = new Fighter("Dustin Poirier",28,7);
+      Fighter fighter4 = new Fighter("Charles Oliveira", 32,8);
+      Fighter fighter5 = new Fighter("Justin Gaethje",23,3);
+      Fighter fighter6 = new Fighter("Kamaru Usman",20,1);
+      Fighter fighter7 = new Fighter("Derrick Lewis",26,9);
+      Fighter fighter8 = new Fighter("Amanda Nunes",21,5);
+      Fighter fighter9 = new Fighter("Holly Holm",14,5);
+      Fighter fighter10 = new Fighter("Jorge Masvidal",35,16);
+
+      ArrayList<Fighter> Fighters = new ArrayList<>();
+      Fighters.add(fighter1);
+      Fighters.add(fighter2);
+      Fighters.add(fighter3);
+      Fighters.add(fighter4);
+      Fighters.add(fighter5);
+      Fighters.add(fighter6);
+      Fighters.add(fighter7);
+      Fighters.add(fighter8);
+      Fighters.add(fighter9);
+      Fighters.add(fighter10);
+
+
+
+   }
 }
